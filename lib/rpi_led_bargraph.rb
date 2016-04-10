4d24705533e19d3bd22472fefab909c0b9620f1d
Your Name <you@example.com>
2016-04-10 23:54:44 +0100

    feature: Brightness can now be adjusted.

--- lib/rpi_led_bargraph.rb
+++ lib/rpi_led_bargraph.rb
@@ -2,25 +2,37 @@
 
 # file: rpi_led_bargraph.rb
 
-require 'rpi'
+require 'rpi_led'
 
 
-class RPiLedBarGraph < RPi
+class RPiLedBarGraph
+  
+  attr_accessor :brightness
 
   def initialize(pins, wait: 0.1, dir: :ltr, invert: false)
-    super pins
+    
+    @pins = pins.map {|x| RPiLed.new x, smooth: false}
     @wait, @dir, @invert = wait, dir, invert
+    @level, @brightness, @blink = 100, 100, false
+    
+  end
+  
+  def brightness=(val)
+    @brightness = val
+    level(@level, blink: @blink)
   end
 
   def on
-    a = @dir == :ltr ? pins : pins.reverse
+    a = @dir == :ltr ? @pins : @pins.reverse
     a.each {|x| x.on; sleep @wait}
+    @old_level = 100
     :on
   end
 
   def off
-    a = @dir == :ltr ? pins.reverse : pins
+    a = @dir == :ltr ? @pins.reverse : @pins
     a.each {|x| x.off; sleep @wait}
+    @old_level = 0
     :off
   end
 
@@ -30,22 +42,41 @@ class RPiLedBarGraph < RPi
 
   # input % from 0-100
   #
-  def level(n, state=nil, blink: false)
+  def level(n, blink: false)
 
-    blink = state if state == :blink
+    @blink = blink
 
-    upper = pins.length / (100.0 / n) - 1
-    upper = pins.length - upper - 1 if @invert
+    upper = (@pins.length / (100.0 / n) - 1).round
+    upper = @pins.length - upper - 1 if @invert
     
-    a = @dir == :ltr ? pins : pins.reverse
+    a = @dir == :ltr ? @pins : @pins.reverse
+    
+    
     a.each(&:off)
-    sleep 0.3 if blink
-    a[0..(upper).to_i].each(&:on)
+    a[0..(upper - 1).to_i].each {|x| x.on; x.brightness = @brightness}
+   
+    if @blink then
+      sleep 0.3
+      a[upper].brightness = @brightness
+      a[upper].blink
+    else
+      a[upper].off
+      sleep 0.3
+      a[upper].brightness = @brightness
+    end
     
-    a[upper].blink if blink
-
+    @level = n
+ 
   end
 
   alias progress level
+  
 
 end
+
+if __FILE__ == $0 then
+  
+
+  g = RPiLedBarGraph.new(%w(26 19 13 6 5 11 20 16 4 12), wait: 0.1)
+  g.on; sleep 0.2; g.off
+
--- rpi_led_bargraph.gemspec
+++ rpi_led_bargraph.gemspec
@@ -1,10 +1,10 @@
 Gem::Specification.new do |s|
   s.name = 'rpi_led_bargraph'
-  s.version = '0.1.0'
+  s.version = '0.2.0'
   s.summary = 'Controls an LED bar graph using a Raspberry Pi'
   s.authors = ['James Robertson']
   s.files = Dir['lib/rpi_led_bargraph.rb']
-  s.add_runtime_dependency('rpi', '~> 0.4', '>=0.4.7')
+  s.add_runtime_dependency('rpi_led', '~> 0.1', '>=0.1.1')
   s.signing_key = '../privatekeys/rpi_led_bargraph.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
